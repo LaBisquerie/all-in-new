0fbc6cd54c5b99bab8b18ac3026d52fe952200ad
LucasDindault <lucas@creabilis.com>
2022-01-10 10:04:48 +0100

    init project

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, {useContext} from 'react'
+import { UserContext } from '../context/UserContext'
+
+export default function Home() {
+
+  const {currentUser} = useContext(UserContext)
+
+  return (
+    <div className='container p-5'>
+      <h1 className='display-3 text-light'>
+        {currentUser ? "Welcome buddy" : "Hi, Sign up or Sign in"}
+      </h1>
+    </div>
+  )
+}
---
+++ src/pages/Private/PrivateHome/PrivateHome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PrivateHome() {
+  return (
+    <div className='container'>
+      <h1>Private Home</h1>
+    </div>
+  )
+}
